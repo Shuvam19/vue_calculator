8d40d46625111b17d83b0ecc84e53fe05b955be7
shuvam kumar <shuvam1982000@gmail.com>
2021-10-16 15:59:03 +0530

    date and bmi design

---
+++ src/components/converter/ConverterBmi.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="converter-bmi">
+    <div class="input-fields">
+      <!-- <input-number /> -->
+      <!-- <input-number /> -->
+    </div>
+    <div class="numpad">
+        <numpad />
+    </div>
+  </div>
+</template>
+
+<script>
+// import InputNumber from "../input/InputNumber.vue";
+import Numpad from '../numpad/Numpad.vue';
+export default {
+  components: {  Numpad },
+};
+</script>
+
+<style>
+.converter-data {
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.input-feilds {
+  width: 100%;
+}
+</style>
--- src/components/converter/ConverterDate.vue
+++ src/components/converter/ConverterDate.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="converter-date">
-    <div class="input-fields"></div>
+    <div class="input-fields">
+      <div class="from-date">
+        <p class="default-input-text">From</p>
+        <input type="date" class="default-input" />
+      </div>
+      <div class="to-date">
+        <p class="default-input-text">To</p>
+        <input type="date" class="default-input" />
+      </div>
+    </div>
+    <div class="difference">
+      <div class="defference-text">Difference</div>
+      <div class="difference-numbers">
+        <div class="difference-years">
+          <p>Years</p>
+          <p>0</p>
+        </div>
+        <div class="difference-months">
+          <p>Months</p>
+          <p>0</p>
+        </div>
+        <div class="difference-days">
+          <p>Days</p>
+          <p>0</p>
+        </div>
+      </div>
+      <div class="difference-from-to">
+        <div class="difference-from">
+          <p>From</p>
+          <p>Oct 16,2021</p>
+        </div>
+        <div class="difference-to">
+          <p>From</p>
+          <p>Oct 16,2021</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,5 +46,111 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.converter-date {
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+}
+
+.input-fields {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.from-date,
+.to-date {
+  padding: 0px 15px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.default-input-text {
+  width: 40%;
+  height: 40px;
+  font-size: 20px;
+  color: white;
+  background-color: #31475e;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+}
+
+.default-input {
+  width: 40%;
+  height: 40px;
+  font-size: 20px;
+  color: white;
+  border: none;
+  background-color: #31475e;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  border-radius: 5px;
+}
+
+.difference {
+  width: 300px;
+  height: 200px;
+  background-color: #31475e;
+  border-radius: 10px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between
+}
+
+.defference-text {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 22px;
+  color: white;
+  font-family: monospace;
+}
+
+.difference-numbers {
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.difference-years,
+.difference-months,
+.difference-days {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 25px;
+  color: white;
+  font-family: monospace;
+}
+
+.difference-from-to {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+
+.difference-from,
+.difference-to {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 18px;
+  color: white;
+  font-family: monospace;
+}
 </style>
